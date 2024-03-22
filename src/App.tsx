15f2d06da5f242f94793ce31b94982dc675e7c8f
@luccabasto <lcbeezus@outlook.com>
2024-03-22 18:27:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+//Components
+import Header from './Components/header/Header'
+
+//Styles
+import './Styles/App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className=''>
+        <Header/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
