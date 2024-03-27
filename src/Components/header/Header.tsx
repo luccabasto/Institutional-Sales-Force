7c1d1087a4c571a73409e228bfc199b17ad452be
@luccabasto <lcbeezus@outlook.com>
2024-03-27 10:50:55 -0300

    Desenvolvimento do Header concluído - faltando apenas realizar o path das seus respectivos componentes quando eles estiverem prontos.

--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -22,6 +22,7 @@ import { HiBarsArrowDown } from 'react-icons/hi2';
 
 function Header() {
     const [toggle, setToggle]=useState(false);
+    const [toggleASIDE, setToggleASIDE] =useState(false);
 
     const menu =[
         {
@@ -39,55 +40,84 @@ function Header() {
         name:'EMPRESA',
         icon:IoBusiness
     },
-    {
-        id:'SUPORTE',
-        name:'SUPORTE',
-        icon:MdSupportAgent
-    }
-   
-]
-    const menuASIDE=[
-        {
-        id:'USER',
-        name:'USER',
-        icon:HiMiniUserCircle 
-    }
     
 ]
 
+    const menuASIDE =[
+
+        {
+            id:'USER',
+            name:'Acesso',
+            icon:HiMiniUserCircle 
+        },
+        {
+            id:'SUPORTE',
+            name:'Suporte',
+            icon:MdSupportAgent
+
+        }
+    ]
   return (
 
     <div className='flex items-center justify-between p-5'>
+        <div className='flex items-center gap-4'>
 
-        <div className='flex items-center gap-8'>
-
-            <img src={header_logo} alt="Logo da SalesForce para o header" className='w-[80px] md:w-[115px] object-cover' />
-
-            <div className=' md:flex gap-8'>
-                {menu.map((item)=>(
-                    <HeaderItem name={item.name} Icon={item.icon} key={item.id}/>
-                ))};
-
-                {/**Responsive menu */}
-                <div className='flex md:hidden gap-5'>
-                    {menu.map((item,index) =>index>3&&(
-                        <HeaderItem name={''} Icon={item.icon} key={item.id}/>
+            {/**Responsive menu */}
+            <div className='flex md:hidden gap-5'>
+                {menu.map((item,index) =>index>2&&(
+                    <HeaderItem name={''} Icon={item.icon} key={item.id}/>
                     ))}
 
-                    <div className='md:hidden' onClick={()=> setToggle(!toggle)}>
-                        <HeaderItem name={''} Icon={HiBars4}/>
-                        {toggle ?
-                            <div className='absolute mt-3 border-[1px] border-gray-700 p-3 px-5 py-4'>
-                                {menu.map((item,index) =>index>1&&(
-                                    <HeaderItem name={item.name} Icon={item.icon} key={index}/>
+                <div className='md:hidden' onClick={()=> setToggle(!toggle)}>
+                    <HeaderItem name={''} Icon={HiBars4}/>
+                    {toggle ?
+                        <div className='absolute mt-3 border-[1px] border-gray-700 p-3 px-5 py-4 rounded-md'>
+                            {menu.map((item,index) =>index<3&&(
+                                <HeaderItem name={item.name} Icon={item.icon} key={index}/>
                                 ))}
                             </div> 
                         :null }
                     </div>
 
-                </div> {/** Responsive Menu */}
+                </div> {/** Responsive Menu */}  
+
+            <img src={header_logo} alt="Logo da SalesForce para o header" className='w-[80px] md:w-[115px] object-cover' />
+
+            <div className='hidden md:flex gap-8'>
+                {menu.map((item)=>(
+                    <HeaderItem name={item.name} Icon={item.icon} key={item.id}/>
+                ))};
             </div>
+
+              
         </div>
+                    
+        <div className=' items-center flex gap-5'> {/**Opções ASIDE */}
+            <div className='hidden md:flex gap-5'>
+                {menuASIDE.map((item)=> (
+                    <HeaderItem name={item.name} Icon={item.icon} key={item.id}/>
+                ))}
+            </div>
+
+            <div className='flex md:hidden gap-5'> {/**Responsive ASIDE */}
+                    {menuASIDE.map((item, index)=>index=0&&(
+                        <HeaderItem name={''} Icon={item.icon} key={item.id}/>
+                    ))}
+
+                    <div className=' md:hidden' onClick={()=> setToggleASIDE(!toggleASIDE)}>
+                        <HeaderItem name={''}  Icon={HiMiniUserCircle}/>
+                        {toggleASIDE ?
+                            <div className={`absolute mt-3 border-[1px] border-gray-700 p-3 px-5 py-4 rounded-md ${window.innerWidth < 768 ? 'right-0' : ''}`}>{menuASIDE.map((item,index) => index<2&&(
+                                    
+                                    <HeaderItem name={item.name} Icon={item.icon} key={index}/>
+                                ))}
+                            </div>
+                        : null}
+
+                    </div>
+
+            </div> {/** Responsive ASIDE */}
+        </div> {/**ASIDE */}
     </div>
   )
 }
