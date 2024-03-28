caa63fbf9cb775b0632a2d5a75f041d590ad1cd1
@luccabasto <lcbeezus@outlook.com>
2024-03-28 04:55:05 -0300

    Estrutura inicial do Slide concluída

--- src/Components/appHero/appH_Components/Slider.tsx
+++ src/Components/appHero/appH_Components/Slider.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import imageSlide from './Slider_Images'
+
+const screenWidth=window.innerWidth
+
+//Icons
+import { HiChevronLeft,HiChevronRight } from 'react-icons/hi2'
+
 
 function Slider() {
+  //buttons 
+  const elementRef=useRef();
+
+  const sliderRight=(element)=>{
+    element.scrollLeft+=screenWidth-110
+}
+
+const sliderLeft=(element)=>{
+    element.scrollLeft-=screenWidth-110
+}
+
   return (
-   <>
-      <div className='text-white'>Slider</div>
-   </>
+   <div>
+    <HiChevronLeft className=" bg-transparent text-gray text-[30px] absolute mx-8 mt-[200px] cursor-pointer hidden lg:block" onClick={()=>sliderLeft(elementRef.current)} />
+
+    <HiChevronRight className=" bg-transparent text-gray text-[30px] absolute mx-8 mt-[200px] right-0 cursor-pointer hidden lg:block" onClick={()=>sliderRight(elementRef.current)} />
+
+    <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth' ref={elementRef}>
+        {imageSlide.map(imagem =>(
+          <img src={imagem.src} alt={imagem.alt} className='min-w-full md:h-[400px] object-fill object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in-out shadow-md shadow-gray-800 ' />
+        ))}
+    </div>
+  </div>
+      
+   
   )
 }
 
---
+++ src/Components/appHero/appH_Components/Slider_Images.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+//images
+import imagem1 from './../../../assets/images/id1.png'
+import imagem2 from './../../../assets/images/id2.jpg'
+import imagem3 from './../../../assets/images/id3.jpg'
+
+const imageSlide= [
+    {
+        id: 1, src: imagem1, alt: 'teste'
+    },
+    {
+        id: 2, src: imagem2, alt: 'teste'
+    },
+    {
+        id: 3, src: imagem3, alt: 'teste'
+    }
+]
+///ADICIONAR MAIS IMAGENS 
+export default imageSlide
