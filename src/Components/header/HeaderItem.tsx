56727dc9953cd2c16f48ea0f17e96df4174b22da
@luccabasto <lcbeezus@outlook.com>
2024-03-30 02:30:30 -0300

    Apenas alteração da cor dos elementos do header para visualização

--- src/Components/header/HeaderItem.tsx
+++ src/Components/header/HeaderItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HeaderItem({name, Icon}) {
   return (
-    <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8 mb-2'>
+    <div className='text-black flex items-center gap-3 text-[15px] font-semibold cursor-pointer hover:underline underline-offset-8 mb-2'>
         <Icon/>
         <h2 className=''>{name}</h2>
     </div>
