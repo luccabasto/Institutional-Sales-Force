92e6a0bfc8d38b2233135cfdc2230380d15f079b
@luccabasto <lcbeezus@outlook.com>
2024-04-01 03:06:09 -0300

    up new branch

--- src/Components/appHero/appH_Components/InfiniteParceiros.tsx
+++ src/Components/appHero/appH_Components/InfiniteParceiros.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function InfiniteParceiros() {
   return (
-    <div>Mais de 150.000 empresas, grandes, médias e pequenas, estão expandindo seus negócios com a Salesforce.</div>
+    <div>Mais de 150.000 empresas, grandes, médias e pequenas, estão expandindo seus negócios com a Salesforce.
+      <p>Subindo brach</p>
+    </div>
   )
 }
